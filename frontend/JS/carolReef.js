71dd5f4d80cffa0d94a44cc264e8e7372764e475
aransagarcia <aransagarcia@pursuit.org>
2019-11-17 16:03:16 -0500

    made several pages with link to specific habitat

---
+++ frontend/JS/carolReef.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadCarolReefSightings();
+
+
+});
+
+
+const loadCarolReefSightings = async () => {
+    const container = document.querySelector('#container');
+
+    const response = await axios.get('http://localhost:3000/sightings/all');
+    console.log(response)
+    for (let i = 0; i < response.data.payload.length; i++) {
+        let sight = response.data.payload[i]
+        if (sight.habitat_id === 2) {
+            let eachSight = document.createElement("div");
+            eachSight.innerText = (`Animal Spotted: ${sight.name}, Researcher: ${sight.researchername}`)
+            eachSight.className = "sight"
+            container.appendChild(eachSight)
+        }
+    }
+}
--- frontend/JS/sightings.js
+++ frontend/JS/sightings.js
@@ -7,14 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 const loadSightings = async () => {
-    const sightingsList = document.querySelector('#allSightings');
-    sightingsList.innerHTML = "";
+    const container = document.querySelector('#container');
+    
     const response = await axios.get('http://localhost:3000/sightings/all');
      for(let i = 0; i < response.data.payload.length; i++){
             let sight = response.data.payload[i]
             let eachSight = document.createElement("div");
             eachSight.innerText = (`Animal Spotted: ${sight.name}, Category: ${sight.category}, Researcher: ${sight.researchername}`)
-            sightingsList.appendChild(eachSight);
+            eachSight.className = "sight"
+            container.appendChild(eachSight);
 }
 }
 
