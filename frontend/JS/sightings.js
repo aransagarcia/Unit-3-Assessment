b2b31c6b50b203fdcd9c6e2739b769465f518d0d
aransagarcia <aransagarcia@pursuit.org>
2019-11-17 17:59:32 -0500

    got to display name of the researcher and job title - still missing the species they have seen

--- frontend/JS/sightings.js
+++ frontend/JS/sightings.js
@@ -1,32 +1,11 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     loadSightings();
-
-
-// });
-
-
-
-// const loadSightings = async () => {
-//     const container = document.querySelector('#container');
-
-//     const response = await axios.get('http://localhost:3000/sightings/all');
-//      for(let i = 0; i < response.data.payload.length; i++){
-//             let sight = response.data.payload[i]
-//             let eachSight = document.createElement("div");
-//             eachSight.innerText = (`Animal Spotted: ${sight.name}, Category: ${sight.category}, Researcher: ${sight.researchername}`)
-//             eachSight.className = "sight"
-//             container.appendChild(eachSight);
-// }
-// }
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
     getResearchers()
 })
 
 
 const getResearchers = async () => {
+    const container = document.querySelector('#container');
+
     let dropdown = document.querySelector('#selectBox')
     const response = await axios.get('http://localhost:3000/researchers/all');
     let researcher = response.data.payload
@@ -35,32 +14,38 @@ const getResearchers = async () => {
         option.innerText = researcher[i].researchername
         dropdown.appendChild(option)
     }
-    getInfo()
+    getResearcherInfo()
 }
 
 
-const getInfo = () => {
+const getResearcherInfo = () => {
     let dropdown = document.querySelector("#selectBox")
     dropdown.addEventListener('change', async (event) => {
         let selectedIndex = dropdown.options.selectedIndex
         console.log(selectedIndex)
         const response = await axios.get(`http://localhost:3000/sightings/researchers/${selectedIndex}`);
+        let name = response.data.payload[0].researchername
+        let jobtitle = response.data.payload[0].job_title
+        console.log(name, jobtitle)
         console.log(response.data.payload)
-        for (let i = 0; response.data.payload.length; i++) {
-            // let name = response.data.payload[i].researchername
-           console.log(response.data.payload[i].job_title)
-        }
-        displayInfo(name, job_title)
+        // for (let i = 0; response.data.payload.length; i++) {
+        //      let name = response.data.payload[i].researchername
+        //      let jobtitle = response.data.payload[i].job_title
+        //    console.log(response.data.payload[i].job_title)
+
+         displayInfo(name, jobtitle)
     })
+   
 }
 
-const displayInfo = (name, job_title) => {
-    console.log(title, releaseYear, description)
+const displayInfo = (name, jobtitle) => {
+    let container = document.querySelector('#container')
+    console.log(name,jobtitle )
     let researcherName = document.createElement('p')
     researcherName.innerText = name
-    let jobtitle = document.createElement('p')
-    jobtitle.innerText = job_title
-    body.appendChild(researcherName)
-    body.appendChild(job_title)
+    let job_title = document.createElement('p')
+    job_title.innerText = jobtitle
+    container.appendChild(job_title)
 }
+
 
